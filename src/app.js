1a95114c8753aa4b90368e159e1223c803a8b61e
Dawn Monroe <deetriplett@gmail.com>
2017-02-28 19:48:23 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-//Main File for Application//
+// ***** Main File for Application ***** //
 'use strict'; //Throws error, for testing
 
 // ***** MODULES *****
 const express     = require('express');
 const app         = express();
 const bodyParser  = require('body-parser');
+const path        = require('path');
+const mongoose    = require('mongoose');
 //const methodOverride = require('method-override');
 
 // ***** CONFIGURATION *****
@@ -18,31 +20,30 @@ app.get('/', function(req, res){
 
 //Set Port Server 3000 or 8080
 app.listen(8080, function(){
-  console.log("Frontend Server is running on port 8080!")
-
+  console.log("Frontend Server is running on port 8080!");
 });
 
-// connect to Mongodb
-// mongoose.connect(db.url);
+// connect to Mongodb  ---- mongoose.connect(db.url);
+mongoose.connect(`mongodb://${config.db.host}/${config.db.dbname}`);
 
-//get all data(guests) of the body (pOST) parameter
+//get all data(guests) of the body (POST) parameter
 
 // **** Parse *****
 
 //parse application json
 app.use(bodyParser.json());
-
-//parse application/ vnd.api as json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//parse application/ vnd.api as json
+//app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 //app.use(methodOverride('X-HTTP-Method-Override'));
 
-// set the static files location /public/img will be /img for users
-//app.use(express.static(__dirname + '/public'));
+// set the static files location
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/node_modules',express.static(path.join(__dirname, '../node_modules')));
 
 
 
