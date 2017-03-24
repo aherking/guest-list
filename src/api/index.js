87ef2e1cee442bffa8bcf69f3502cf9537881851
Dawn Monroe <deetriplett@gmail.com>
2017-03-23 21:45:44 -0400

    added mongo db

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,31 @@
-//API CONTROLLER
+//API module
 
 'use strict';
+//Express Server
+const express= require('express');
+//Require Guest Model
+const guest = require('../models/guest');
+//const guest = require('../mock/guests.json'); //Import Mock data into API Controller
 
-const express = require('express';
-const guests = require('../../mock/guests.json');
-
+//API Routes
 const router = express.Router();
-
-router.get('/guests', function(req, res) {
+//GET method to get guests
+router.get('/model/guests', function(req, res) {
+//Guest Model use find method
+Guest.find({}, function(err, todos) {
+//Guest.find ({rsvpd:''})
+  if(err) {
+    //console.log(err);
+    return res.status(500).json({message: err.message});//internal server error
+  }
+  // No err, respond with our Guests as a JSON object
   res.json({guests: guests});
+  });
 });
 
-//POST
-//PUT
-//delete
+//Guest: Post route to create new guest
+//Put ROute to update existing entries
+//Delete route to delete guest
 
+//Export router
 module.exports = router;
