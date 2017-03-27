0a457f03cf559e317dfc19114748f80939e48340
Dawn Monroe <deetriplett@gmail.com>
2017-03-27 16:36:15 -0400

    added saveGuests and resetGuestList

--- public/scripts/controllers/guest.js
+++ public/scripts/controllers/guest.js
@@ -1,6 +1,8 @@
 'use strict';
+const angular = require('angular');
 //guestCtrl that deletes and saves
-angular.module('listApp')
+
+angular.module('guestListApp')
 .controller('guestCtrl', function($scope, dataService) {
   $scope.deleteGuest = function(guest, index) {
     $scope.guests.splice(index, 1);
@@ -8,11 +10,19 @@ angular.module('listApp')
   };
 
   $scope.saveGuests = function() {
-    let filteredGuests = $scope.guests.filter(function(guest){
+    var filteredGuests = $scope.guests.filter(function(guest){
       if(guest.edited) {
         return guest
       };
     })
-    dataService.saveGuests(filteredGuests);
+    dataService.saveGuests(filteredGuests)//returns a promise, then resets
+    .finally($scope.resetGuestState());
   };
+  //Reset Guest List state to false
+  $scope.resetGuestState = function() {
+    $scope.guests.forEach(function(guest) {
+      guest.edited = false;
+    });
+  };
+
 });
