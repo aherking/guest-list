5ea478f374bf379b7bc822ff43b587ca096399a2
Dawn Monroe <deetriplett@gmail.com>
2017-03-27 16:49:48 -0400

    Create guest.js

---
+++ src/models/guest.js
@@ -0,0 +1,20 @@
+//Model for mongoose(db = guestlist)
+'use strict';
+const mongoose = require('mongoose');
+//guest.name
+//guest.rsvpd
+
+//Type of data that can be saved
+const guestSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  }
+  // rsvpd: Boolean
+});
+
+//Guest model method
+const Guest = mongoose.model('Guest', guestSchema);
+
+module.exports = guest;
