077b2286332835d5fc9e77728c7a5cf3b0f98da0
Dawn Monroe <deetriplett@gmail.com>
2017-03-27 16:52:30 -0400

    Update database.js

--- src/database.js
+++ src/database.js
@@ -4,11 +4,23 @@
 
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/project', function(err){
-  //callback for connection
-  if(err) {
-    console.log('Failed connecting to Mongodb!');
-  } else {
-    console.log('Success! Connected to Mongodb!');
-  }  
+mongoose.connect('mongodb://localhost:27017/guestlist');
+
+const db = mongoose.connection;
+
+//Error handling with db
+db.on("error", function(err){
+  console.log('Failed connecting to Mongodb!');
 });
+//DB handling with NO ERRORS
+db.once("open", function(){
+  console.log('Success! Connected to Mongodb!');
+  //All db communication goes here
+  const Guest = mongoose.model('Guest', guestSchema);
+  db.close();
+});
+
+
+
+
+module.exports = mongoose;
