c0bb6bd952cd5228cf5d65696ab8852caa21aab4
Dawn Monroe <deetriplett@gmail.com>
2017-03-27 16:34:53 -0400

    Update guest.js

--- public/scripts/directives/guest.js
+++ public/scripts/directives/guest.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const angular = require('angular');
+//on click event, replace <h1> with guest.html template
+
 //Guest directive
-angular.module('listApp')
+angular.module('guestListApp')
 .directive('guest', function(){
   return {
     templateUrl: 'templates/guest.html',
