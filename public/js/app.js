46d3b9cc0737696313722c122d7c34a9eaa8df02
Dawn Monroe <deetriplett@gmail.com>
2017-03-27 16:12:22 -0400

    Delete app.js

--- public/js/app.js
+++
@@ -1,9 +0,0 @@
-//Angular Creates Guest List, see the empty array?
-angular.module("guruGuestList", [])
-.controller('mainCtrl', function($scope) {
-  $scope.list = function() {
-    console.log("This is the list(mainCtrl) controller");
-  };
-});
-
-//add new categories controllers
