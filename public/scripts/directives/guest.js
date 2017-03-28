ab0ca6f94cb58879871ae043c656cbf8de10c78c
Dawn Monroe <deetriplett@gmail.com>
2017-03-28 16:02:54 -0400

    added GuestDirective

--- public/scripts/directives/guest.js
+++ public/scripts/directives/guest.js
@@ -1,14 +1,12 @@
 'use strict';
 
-const angular = require('angular');
-//on click event, replace <h1> with guest.html template
-
 //Guest directive
-angular.module('guestListApp')
-.directive('guest', function(){
+function GuestDirective () {
   return {
     templateUrl: 'templates/guest.html',
     replace: true,
     controller: 'guestCtrl'
   }
-});
+}
+
+module.exports = GuestDirective;
