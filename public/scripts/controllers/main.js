6f13ca1338a977767e4504d6b83ca16e7158f74c
Dawn Monroe <deetriplett@gmail.com>
2017-03-28 15:59:39 -0400

    created MainCtrl function

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,18 +1,17 @@
 'use strict';
 
 //mainCtrl
-angular.module('listApp')
-.controller('mainCtrl', function($scope, dataService){
-
-// gets guests and attaches list to scopes guests variable
+function MainCtrl ($scope, dataService) {
+// gets guests and attaches list to scope.guests variable
   dataService.getGuests(function(response){
-    let guests = response.data.guests;//add guest
+    let guests = response.data.guests;
     $scope.guests =  guests;
     });
-// Create new guest and add it to guest list
+// Create new guest and adds it to guest list using unshift
   $scope.addGuest = function() {
     $scope.guests.unshift({name: "Who would you like to invite?",
                       rsvpd: false});
   };
+}
 
-})
+module.exports = MainCtrl;
