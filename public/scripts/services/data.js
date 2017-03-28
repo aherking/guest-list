5849808b821bb5d53538dca252ac46147e309c6c
Dawn Monroe <deetriplett@gmail.com>
2017-03-28 16:04:23 -0400

    refactor to function DataService

--- public/scripts/services/data.js
+++ public/scripts/services/data.js
@@ -1,42 +1,41 @@
 'use strict';
 
-const angular = require('angular');
-
-angular.module('guestListApp')
-.service('dataService', function($http, $q) {//$q helps manage requests by resolving all promises before returning results
-  //GET GUEST DATA
+//$q helps manage requests by resolving all promises before returning results
+function DataService ($http, $q) {
+  //GET
   this.getGuests = function(cb) {
-    //Route set in src/index.js, $http is angular service to communicate with API
     $http.get('/api/guests').then(cb);
   };
-  
-  //DELETE GUEST FROM LIST
+  //DELETE
   this.deleteGuest = function(guest) {
-    console.log("Deleted " + guest.name);
+    if(!guest._id) {
+      return $q.resolve();
+    }
+    return $http.delete('api/guests' + guest._id).then(function(){
+        console.log("Deleted " + guest.name);
+    });
   };
-
-  //SAVE GUEST DATA (POST/PUT)
+  //SAVE
   this.saveGuests = function(guests) {
-    let queue = [];//Empty array to store Guest Data in...
-    //Loop thru each guest--for each guest, push a request to the 'queue'
+    let queue = [];
+    //Loop thru each guest--for each guest, push a req to the queue
     guests.forEach(function(guest) {
       let request;
       //if guest does not have id...
       if (!guest._id) {
-        request = $http.post('/api/guests', guest);
+        request = $http.post('/api/guests', guest)
       } else {
         request = $http.put('/api/guests/' + guest._id , guest).then(function(result){
           let guest = result.data.guest;
           return guest;
         });
-      };
-      //Push a request to the 'queue'
+      }
       queue.push(request);
     });
-    
       return $q.all(queue).then(function(results) {
       console.log("Guest total: " + guests.length);
     });
   };
+}
 
-});//End dataService
+module.exports = DataService;
