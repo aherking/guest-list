64d3c22b12a8f7c1e4a2d66527c7860e956346d0
Dawn Monroe <deetriplett@gmail.com>
2017-03-28 15:58:53 -0400

    Create index.js

---
+++ public/scripts/controllers/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const angular = require('angular');
+
+angular.module('guestListApp').controller('mainCtrl', require('./main'));
+angular.module('guestListApp').controller('guestCtrl', require('./guest'));
