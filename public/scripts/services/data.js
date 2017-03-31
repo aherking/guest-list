85d454ae64f21295e43ced3b78d6a5fe88b6dd7a
Dawn Monroe <deetriplett@gmail.com>
2017-03-31 10:59:23 -0400

    Update data.js
    Added Angular Mode. (Uncomment Mongo Mode code, and comment out Angular Mode to integrate app with Mongo)

--- public/scripts/services/data.js
+++ public/scripts/services/data.js
@@ -1,41 +1,70 @@
 'use strict';
 
 angular.module('guestListApp')
-.service('dataService', function($http, $q){//$q helps manage requests by resolving all promises before returning results
-//GET
+//------- ANGULAR MODE------- //
+.service('dataService', function($http){
+
+         this.helloWorld = function() {
+          console.log("dataService Method");
+};
+
 this.getGuests = function(callback) {
-  $http.get('/api/guests')
-  .then(callback);
-};
-//DELETE
-this.deleteGuest = function(guest) {
-  if(!guest._id) {
-    return $q.resolve();
+  $http.get('mock/guests.json')
+  .then(callback)
   }
-  return $http.delete('api/guests' + guest._id).then(function(){
-    console.log(guest.name +" has been deleted");
-  });
+
+this.deleteGuest = function(guest) {
+  console.log(guest.name +" has been deleted");
+  //other logic
 };
-//SAVE (post/put)
+
 this.saveGuests = function(guest) {
-let queue = [];
-//Loop thru each guest--for each guest, push a req to the queue
-guests.forEach(function(guest) {
-  let request;
-  //if guest does not have id...
-  if(!guest._id) {
-    request = $http.post('/api/guests', guest);
-  } else {
-    request = $http.put('/api/guests/' + guest._id, guest).then(function(result){
-      let guest = result.data.guest;
-      return guest;
-    })
-  };
-  queue.push(request);
-});
-return $q.all(queue).then(function(results){
-  console.log("Total guests saved: " + guest.length );
-});
+  console.log("Total guest count: " + guest.length );
 };
 
 });
+
+//------- MONGO MODE------- //
+// .service('dataService', function($http, $q){//$q helps manage requests by resolving all promises before returning results
+// //GET
+// this.getGuests =
+//   $http.get('/api/guests')
+//   //$http.get('mock/guests.json')//so it 100% works
+//   .then(function(response){
+//     console.log(response.data);
+//     return response.data;
+//   });
+// };
+//
+// //DELETE
+// this.deleteGuest = function(guest) {
+//   if(!guest._id) {
+//     return $q.resolve();
+//   }
+//     return $http.delete('api/guests' + guest._id).then(function(){
+//     console.log(guest.name +" has been deleted");
+//   });
+// };
+//
+// //SAVE (post/put)
+// this.saveGuests = function(guest) {
+// let queue = [];
+// //Loop thru each guest--for each guest, push a req to the queue
+// guests.forEach(function(guest) {
+//   let request;
+//   //if guest does not have id...
+//   if(!guest._id) {
+//     request = $http.post('/api/guests', guest);
+//   } else {
+//     request = $http.put('/api/guests/' + guest._id, guest).then(function(result){
+//       let guest = result.data.guest;
+//       return guest;
+//     });
+//   };
+//   queue.push(request);
+// });
+// return $q.all(queue).then(function(results){
+//   console.log("Total guests saved: " + guest.length );
+// });
+// }
+// });
