0f5020e7927cfa867ae1e5f2a51489c309aece05
Dawn Monroe <deetriplett@gmail.com>
2017-03-31 11:06:41 -0400

    Update database.js

--- src/database.js
+++ src/database.js
@@ -12,15 +12,23 @@ const db = mongoose.connection;
 
 //Error handling with db
 db.on("error", function(err){
-  console.log('Failed! No connection to Mongodb!');
+  console.log('Eek! Your Mongodb is not connected!');
 });
 //DB handling with NO ERRORS
 db.once("open", function(){
-  console.log('Success! Connected to Mongodb!');
+  console.log('Nice. Your Mongodb connection is looking good!');
   //All db communication goes here
 
   db.close();
 });
+
+// mongoose.connect('mongodb://localhost/guestlist', function(error){
+//   if(error){
+//     console.log("Eek! Your Mongodb is not connected!");
+//   } else {
+//     console.log("Nice. Your Mongodb connection is looking good!");
+//   }
+// });
 
 
 
