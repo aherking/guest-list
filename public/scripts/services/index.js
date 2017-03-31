7458e19d541a0b19434c36cb45598dfefa85b3fd
Dawn Monroe <deetriplett@gmail.com>
2017-03-31 00:03:30 -0400

    Delete index.js

--- public/scripts/services/index.js
+++
@@ -1,6 +0,0 @@
-//public/scripts/services/index.js
-'use strict';
-const angular = require('angular');
-
-//Add dataService to app
-angular.module('guestListApp').service('dataService', require('./data'));
