cfd6e05998090312972e22d92d3f91532bd87888
Dawn Monroe <deetriplett@gmail.com>
2017-03-31 00:10:36 -0400

    Update app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,5 @@
 'use strict';
-const angular = require('angular');
 
 //Create a new module "guestListApp" (with an empty array)
 angular.module('guestListApp', []);
 
-//Add services/directives/ and controllers to app
-require('./scripts/services');
-require('./scripts/directives');
-require('./scripts/controllers');
