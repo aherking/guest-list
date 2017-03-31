3a57edfea072f756b843858f9fabe03a135893a6
Dawn Monroe <deetriplett@gmail.com>
2017-03-30 23:36:01 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,4 @@
-
-'use strict'
-
+'use strict';
 const express = require('express');
 const config  = require('./config');
 const parser = require('body-parser');
@@ -8,10 +6,8 @@ const router = require('./api'); //Require API Module (mounted to router)
 const path = require('path');
 
 const app = express();
-//Connect database to app
-require('./database');
-//Connect seed data
-require('./seed');
+require('./database');//Connect database to app
+require('./seed');//Connect seed data
 
 // let jsonCheck = function(req, res){
 //   if (req.body) {
@@ -21,20 +17,17 @@ require('./seed');
 //   }
 //   next();
 // }
-require('./models/guest.js');
-
 //Serving entire Public folder (the Face of the App) at root url
 app.use('/', express.static('public'));
 app.use(parser.json());//JSON body-parser
 app.use(parser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 // app.use(jsonCheck);
-app.use(express.static(path.join(__dirname, '../public')));
 app.use('/node_modules',express.static(path.join(__dirname, '../node_modules')));
 
 
-//API Name Space added to router, so it'll be automatically prefixed
+//API Namespace added to router, so it'll be automatically prefixed
 app.use('/api', router);
 
 app.listen(8080, function() {
-  console.log("Bears. Beets. Battlestar Galactica...and server is running on port 8080!");
+  console.log("Bears. Beets. Battlestar Galactica. We hear ya on port 8080!");
 });
