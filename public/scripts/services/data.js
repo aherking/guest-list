824f76d12d18a340d5044eb08e2280e1f6555bc4
Dawn Monroe <deetriplett@gmail.com>
2017-03-31 00:09:41 -0400

    Update data.js

--- public/scripts/services/data.js
+++ public/scripts/services/data.js
@@ -1,41 +1,41 @@
 'use strict';
 
-//$q helps manage requests by resolving all promises before returning results
-function DataService ($http, $q) {
-  //GET
-  this.getGuests = function(cb) {
-    $http.get('/api/guests').then(cb);
+angular.module('guestListApp')
+.service('dataService', function($http, $q){//$q helps manage requests by resolving all promises before returning results
+//GET
+this.getGuests = function(callback) {
+  $http.get('/api/guests')
+  .then(callback);
+};
+//DELETE
+this.deleteGuest = function(guest) {
+  if(!guest._id) {
+    return $q.resolve();
+  }
+  return $http.delete('api/guests' + guest._id).then(function(){
+    console.log(guest.name +" has been deleted");
+  });
+};
+//SAVE (post/put)
+this.saveGuests = function(guest) {
+let queue = [];
+//Loop thru each guest--for each guest, push a req to the queue
+guests.forEach(function(guest) {
+  let request;
+  //if guest does not have id...
+  if(!guest._id) {
+    request = $http.post('/api/guests', guest);
+  } else {
+    request = $http.put('/api/guests/' + guest._id, guest).then(function(result){
+      let guest = result.data.guest;
+      return guest;
+    })
   };
-  //DELETE
-  this.deleteGuest = function(guest) {
-    if(!guest._id) {
-      return $q.resolve();
-    }
-    return $http.delete('api/guests' + guest._id).then(function(){
-        console.log("Deleted " + guest.name);
-    });
-  };
-  //SAVE
-  this.saveGuests = function(guests) {
-    let queue = [];
-    //Loop thru each guest--for each guest, push a req to the queue
-    guests.forEach(function(guest) {
-      let request;
-      //if guest does not have id...
-      if (!guest._id) {
-        request = $http.post('/api/guests', guest)
-      } else {
-        request = $http.put('/api/guests/' + guest._id , guest).then(function(result){
-          let guest = result.data.guest;
-          return guest;
-        });
-      }
-      queue.push(request);
-    });
-      return $q.all(queue).then(function(results) {
-      console.log("Guest total: " + guests.length);
-    });
-  };
-}
+  queue.push(request);
+});
+return $q.all(queue).then(function(results){
+  console.log("Total guests saved: " + guest.length );
+});
+};
 
-module.exports = DataService;
+});
