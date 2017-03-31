2a8df863ebc7cbf652d0316cb640c6f0fefa1f33
Dawn Monroe <deetriplett@gmail.com>
2017-03-31 00:01:27 -0400

    Delete index.js

--- public/scripts/controllers/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const angular = require('angular');
-
-angular.module('guestListApp').controller('mainCtrl', require('./main'));
-angular.module('guestListApp').controller('guestCtrl', require('./guest'));
