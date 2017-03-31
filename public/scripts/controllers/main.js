a86dda1a7c7b0d2232fe879a8691980ad7b1e659
Dawn Monroe <deetriplett@gmail.com>
2017-03-31 10:57:09 -0400

    Update main.js
    Added Angular Mode. Uncomment Mongo mode to use mongo code (and then comment out angular)

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,16 +1,47 @@
 'use strict';
 
 angular.module('guestListApp')
+
+//------- ANGULAR MODE------- //
 .controller('mainCtrl', function($scope, dataService) {
-  //ADD new guest data
   $scope.addGuest = function() {
-    let guest = {name: "New Guest", rsvpd: false};
+    let guest = {name: "Who would you like to invite?", rsvpd: false};
     $scope.guests.unshift(guest);
   };
-//GET guest data and attaches it to list
+
+
 dataService.getGuests(function(response) {
   console.log(response.data);
-  let guests = response.data.guests;
-  $scope.guests =  guests;
+  $scope.guests =  response.data;
 });
-});
+
+  $scope.deleteGuest = function(guest, $index) {
+    dataService.deleteGuest(guest);
+    $scope.guests.splice($index, 1);
+  };
+
+  $scope.saveGuests = function() {
+    let filteredGuests = $scope.guests.filter(function(guest){
+      if(guest.edited) {
+        return guest;
+      };
+    });
+    dataService.saveGuests(filteredGuests);
+  };
+})
+
+
+//------- MONGO MODE------- //
+// .controller('mainCtrl', function($scope, dataService) {
+//   //ADD new guest data
+//   $scope.addGuest = function() {
+//     let guest = {name: "New Guest", rsvpd: false};
+//     $scope.guests.unshift(guest);
+//   };
+// //GET guest data and attaches it to list
+// dataService.getGuests(function(response) {
+//   console.log(response.data);
+//   let guests = response.data.guests;
+//   $scope.guests =  guests;
+// });
+// });
