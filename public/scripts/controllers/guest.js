c33a70b9555be60262a91af95f2835b575ee9d17
Dawn Monroe <deetriplett@gmail.com>
2017-03-31 10:55:17 -0400

    Update guest.js
    This file is used in "mongo mode". Uncomment code to use.

--- public/scripts/controllers/guest.js
+++ public/scripts/controllers/guest.js
@@ -1,28 +1,27 @@
-'use strict';
-//guestCtrl that deltes and saves guests
-angular.module('guestListApp')
-.controller('guestCtrl', function($scope, dataService) {
-  //DELETE
-    $scope.deleteGuest = function(guest, $index) {
-      dataService.deleteGuest(guest);
-      $scope.guests.splice($index, 1);
-    };
-  //Save List (saveGuest ctrl)
-    $scope.saveGuests = function() {
-      let filteredGuests = $scope.guests.filter(function(guest){
-        if(guest.edited) {
-          return guest;
-        };
-      })
-      dataService.saveGuests(filteredGuests);//returns a promise , then resets
-    };
+//------ MONGO MODE -----//
 
-    $scope.resetGuestState = function() {
-      $scope.guests.forEach(function(guest){
-        guest.rsvpd = false;
-      });
-    };
-});
-
-
- 
+// 'use strict';
+// //guestCtrl that deltes and saves guests
+// angular.module('guestListApp')
+// .controller('guestCtrl', function($scope, dataService) {
+//   //DELETE
+//     $scope.deleteGuest = function(guest, $index) {
+//       dataService.deleteGuest(guest);
+//       $scope.guests.splice($index, 1);
+//     };
+//   //Save List (saveGuest ctrl)
+//     $scope.saveGuests = function() {
+//       let filteredGuests = $scope.guests.filter(function(guest){
+//         if(guest.edited) {
+//           return guest;
+//         };
+//       })
+//       dataService.saveGuests(filteredGuests);//returns a promise , then resets
+//     };
+//
+//     $scope.resetGuestState = function() {
+//       $scope.guests.forEach(function(guest){
+//         guest.rsvpd = false;
+//       });
+//     };
+// });
